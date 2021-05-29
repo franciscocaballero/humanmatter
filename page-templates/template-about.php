f567e4a1fd17e7c7f3b9b124a586a2325bda929f
franciscocaballero <caballerofrancisco22@yahoo.com>
2021-05-29 00:37:49 -0400

    styling about page

--- page-templates/template-about.php
+++ page-templates/template-about.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Full Width Page
+ * Template Name: Template About
  *
  * Template for displaying a page without sidebar even if a sidebar widget is published.
  *
@@ -17,38 +17,76 @@ if ( is_front_page() ) {
 	get_template_part( 'global-templates/hero' );
 }
 ?>
+<!-- <div class="wrap">
 
-<div class="wrapper" id="full-width-page-wrapper">
+    <section class="section-about align-items-center">
 
-	<div class="<?php echo esc_attr( $container ); ?>" id="content">
 
-		<div class="row">
+        <div class="container col-10">
+            <div class="row">
+                <div class="col-6">
 
-			<div class="col-md-12 content-area" id="primary">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pellentesque ullamcorper orci in
+                        morbi. Semper tellus lacus sodales lectus turpis mattis. Habitasse turpis semper ut amet
+                        volutpat, tortor, et. A duis justo et at nibh purus adipiscing duis.Lorem ipsum dolor sit amet,
+                        consectetur adipiscing elit. Nulla pellentesque ullamcorper orci in morbi. Semper tellus lacus
+                        sodales lectus turpis mattis. Habitasse turpis semper ut amet volutpat, tortor, et. A duis justo
+                        et at nibh purus adipiscing duis.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                        pellentesque ullamcorper orci in morbi. Semper tellus lacus sodales lectus turpis mattis.
+                        Habitasse turpis semper ut amet volutpat, tortor, et. A duis justo et at nibh purus adipiscing
+                        duis.
 
-				<main class="site-main" id="main" role="main">
+                    </p>
+                </div>
+            </div>
 
-					<?php
-					while ( have_posts() ) {
-						the_post();
-						get_template_part( 'loop-templates/content', 'page' );
+        </div>
 
-						// If comments are open or we have at least one comment, load up the comment template.
-						if ( comments_open() || get_comments_number() ) {
-							comments_template();
-						}
-					}
-					?>
 
-				</main><!-- #main -->
+    </section>
+    <div class="container">
 
-			</div><!-- #primary -->
+        <h1 class="h1-about">Our Story</h1>
+        <p class="p-about">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pellentesque ullamcorper orci in morbi. Semper
+            tellus lacus sodales lectus turpis mattis. Habitasse turpis semper ut amet volutpat, tortor, et. A duis justo et
+            at nibh purus adipiscing duis.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pellentesque
+            ullamcorper orci in morbi. Semper tellus lacus sodales lectus turpis mattis. Habitasse turpis semper ut amet
+            volutpat, tortor, et. A duis justo et at nibh purus adipiscing duis.Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Nulla pellentesque ullamcorper orci in morbi. Semper tellus lacus sodales lectus turpis mattis.
+            Habitasse turpis semper ut amet volutpat, tortor, et. A duis justo et at nibh purus adipiscing duis.
+            
+        </p>
+    </div>
 
-		</div><!-- .row end -->
+</div> -->
 
-	</div><!-- #content -->
-
-</div><!-- #full-width-page-wrapper -->
-
+<section class="section-about align-items-center">
+    <div class="container col-10">
+        <div class="row align-items-center">
+            <div class="col-xl-6 col-lg-12">
+                <h1 class="margin-bottom">Our Story</h1>
+                
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pellentesque ullamcorper orci in
+                    morbi. Semper tellus lacus sodales lectus turpis mattis. Habitasse turpis semper ut amet
+                    volutpat, tortor, et. A duis justo et at nibh purus adipiscing duis.Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit. Nulla pellentesque ullamcorper orci in morbi. Semper tellus lacus
+                    sodales lectus turpis mattis. Habitasse turpis semper ut amet volutpat, tortor, et. A duis justo
+                    et at nibh purus adipiscing duis.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                    pellentesque ullamcorper orci in morbi. Semper tellus lacus sodales lectus turpis mattis.
+                    Habitasse turpis semper ut amet volutpat, tortor, et. A duis justo et at nibh purus adipiscing
+                    duis.
+                    
+                </p>
+            </div>
+            
+            <div class="pic-sec">
+            </div>
+            <!-- <div class="col-6"> -->
+                <!-- </div> -->
+                
+            </div>
+            
+        </div>
+</section>
 <?php
 get_footer();
